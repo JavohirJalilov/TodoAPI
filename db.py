4dbf02e87224b08f223c8b8237adcbe891996d1b
Jalilov <javohirjalilov777@gmail.com>
2021-06-25 00:29:45 +0500

    add task count ID update ID

--- db.py
+++ db.py
@@ -3,6 +3,25 @@ from tinydb import TinyDB,Query
 User = Query()
 
 db = TinyDB('db.json')
+todo_list = db.table('todoList')
+count_table = db.table('Count')
 
 def read():
-	return {}
+	data = todo_list.all()
+	todo = {"TODO_LIST":data}
+	return todo
+
+def addTask(ID:int,status:bool,task_name:str,task_description:str):
+	todo_list.insert({
+		"id":ID,
+		"status":status,
+		"task_name":task_name,
+		"task_description":task_description
+		})
+
+def countUpdate(ID):
+	count_table.update({'count':ID},User.language=='python')
+
+def count():
+    i = count_table.all()[0]['count']
+    return i
