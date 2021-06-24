68586c051e14da9563fe689750dc083ef3021924
Jalilov <javohirjalilov777@gmail.com>
2021-06-25 00:30:59 +0500

    add add_task fuction

--- todoAPI/views.py
+++ todoAPI/views.py
@@ -4,3 +4,14 @@ from django.http import JsonResponse
 import db
 def read(request):
 	return JsonResponse(db.read())
+
+
+def add_task(request):
+	ID = db.count()
+	print(ID)
+	task_name = request.GET.get('name')
+	task_description = request.GET.get('des')
+	status = False
+	db.addTask(ID,status,task_name,task_description)
+	db.countUpdate(ID+1)
+	return JsonResponse({"addTask":"Done✅"})
